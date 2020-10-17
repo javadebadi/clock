05390b2fbb65b3ebb7b123b9ce1e80df7cd8137d
javadebadi <javad.ebadi.1990@gmail.com>
2020-10-17 17:31:05 +0330

    Bug fix: there was a problem in pervious approch when the handles were getting to 0 second, 0 minute or 0 hour. We fixed this problem with this new approach with function updateClockWithSteps()

--- js/script.js
+++ js/script.js
@@ -2,23 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
-function updateClock(){
-  hourMinuteSecondPos = findPosNow();
-
-  let hourPos = hourMinuteSecondPos[0];
-  let minutePos = hourMinuteSecondPos[1];
-  let secondPos = hourMinuteSecondPos[2];
-
-  HOURHAND.style.transform = "rotate(" + hourPos + "deg)";
-  MINUTEHAND.style.transform = "rotate(" + minutePos + "deg)";
-  SECONDHAND.style.transform = "rotate(" + secondPos + "deg)";
-}
-
-
-
-
-
 function findTimeNow(){
   let date = new Date();
   let hour = date.getHours();
@@ -26,7 +9,6 @@ function findTimeNow(){
   let second = date.getSeconds();
   return [hour, minute, second];
 };
-
 
 function findHourPos(hour, minute, second){
   let tempHour = hour;
@@ -52,6 +34,40 @@ function findPosNow(){
   return [hourPos, minutePos, secondPos];
 };
 
-updateClock();
+function updateClock(){
+  hourMinuteSecondPos = findPosNow();
+
+  let hourPos = hourMinuteSecondPos[0];
+  let minutePos = hourMinuteSecondPos[1];
+  let secondPos = hourMinuteSecondPos[2];
+
+  HOURHAND.style.transform = "rotate(" + hourPos + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minutePos + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secondPos + "deg)";
+
+  return [hourPos, minutePos, secondPos];
+}
+
+var timePos = updateClock();
+var hourPos = timePos[0];
+var minutePos = timePos[1];
+var secondPos = timePos[2];
+
+
 // update clock every 1000 miliseconds
-var loopClock = setInterval(updateClock, 1000);
+//var loopClock = setInterval(updateClock, 1000);
+
+// instead of using Date object everytime
+// we can increment postion of handles every second
+function updateClockWithSteps(){
+
+  hourPos += 1.0/(60*60*12)*360;
+  minutePos += 1.0/(60*60)*360;
+  secondPos += 1.0/(60)*360;
+
+  HOURHAND.style.transform = "rotate(" + hourPos + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minutePos + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secondPos + "deg)";
+};
+
+var loopClock = setInterval(updateClockWithSteps, 1000);
