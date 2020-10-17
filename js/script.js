6c19ed9cd5278901a28c4f6e8c0f4b917360246e
javadebadi <javad.ebadi.1990@gmail.com>
2020-10-17 15:47:28 +0330

    Update: added variables to determine position of handles in clock Added javascript codes to use this position to update css of handles in svg clock Bug Fix: there was a bug in minute selector; that bug get fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("minute");
+const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+let hourPos = 1;
+let minutePos = 10;
+let secondPos = 100;
+
+HOURHAND.style.transform = "rotate(" + hourPos + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minutePos + "deg)";
+SECONDHAND.style.transform = "rotate(" + secondPos + "deg)";
